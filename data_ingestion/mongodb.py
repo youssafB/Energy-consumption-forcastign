976d99c6fdcdc031e21ec9e9457faa9ea2c9a177
youss@f <119492948+youssafB@users.noreply.github.com>
2025-03-14 14:24:50 +0100

    Update mongodb.py

--- data_ingestion/mongodb.py
+++ data_ingestion/mongodb.py
@@ -1,1 +1,10 @@
+class MongoDBClient:
+    def __init__(self, uri, db_name, collection_name):
+        from pymongo import MongoClient
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+
+    def fetch_all_data(self):
+        return list(self.collection.find({}))  # Fetch all documents
 
